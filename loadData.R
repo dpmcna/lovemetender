292f3c4ae178254f40cf87fccca4339c90f54178
Ubuntu <ubuntu@ip-172-31-31-42.ap-southeast-2.compute.internal>
2013-06-01 04:49:28 +0000

    Completed 2012 summary

--- loadData.R
+++ loadData.R
@@ -1,11 +1,39 @@
+makeSummary<-function(x){
+data.frame(
+Value=max(x$Value),
+Description=x$Description[which.max(x$Value)],
+Procurement.Method=x$Procurement.Method[which.max(x$Value)],
+Title=x$Title[which.max(x$Value)],
+Common.Title=names(which.max(table(x$Title))),
+Count=nrow(x),
+Total=sum(x$Value),
+Limited=sum(x[x$Procurement.Method=="Limited tender","Value"]),
+Open=sum(x[x$Procurement.Method=="Open tender","Value"]),
+Prequalified=sum(x[x$Procurement.Method=="Prequalified tender","Value"])
+)
+}
+
 #library(plyr)
 #minValue<-1000000
 #data<-read.csv("2012.csv",stringsAsFactors=FALSE)
 #data[is.na(data)]<-""
-#abnValues<-ddply(data,"ABN",function(x){sum(x$Value)})
-#abnValues<-abnValues[order(-abnValues$V1),]
-abnValues<-data.frame(abnValues,masterName=sapply(abnValues$ABN,function(x){names(which.max(table(data[data$ABN==x,"Supplier.Name"])))}))
-abnValues<-data.frame(Agency.Name="All",abnValues)
-agencyValues<-ddply(data,"Agency.Name",function(x){sum(x$Value)})
-agencyValues<-data.frame(Agency.Name=agencyValues$Agency.Name,ABN="All",V1=agencyValues$V1)
-values<-ddply(data,.(Agency.Name,ABN),function(x){sum(x$Value)})
+
+#abnValues<-ddply(data,"ABN",makeSummary)
+#abnValues<-data.frame(abnValues,Supplier.Name=sapply(abnValues$ABN,function(x){names(which.max(table(data[data$ABN==x,"Supplier.Name"])))}))
+#abnValues<-data.frame(Agency.Name="All",abnValues)
+#abnValues<-abnValues[!abnValues$ABN=="",]
+
+#agencyValues<-ddply(data,"Agency.Name",makeSummary)
+#agencyValues<-data.frame(Agency.Name=agencyValues$Agency.Name,ABN="All",agencyValues[,-1])
+#agencyValues<-data.frame(agencyValues,Supplier.Name="All")
+
+#values<-ddply(data,.(Agency.Name,ABN),makeSummary)
+#values<-values[!values$ABN=="",]
+#values<-merge(values,abnValues[,c("Supplier.Name","ABN")],by="ABN")
+#values<-data.frame(Agency.Name=values$Agency.Name,ABN=values$ABN,values[,c(-1,-2)])
+
+#totalValues<-data.frame(Agency.Name="All",ABN="All",makeSummary(data),Supplier.Name="All")
+
+#allValues<-data.frame(rbind(totalValues,agencyValues,abnValues,values))
+save(allValues,file="loadData.RData")
+write.csv(allValues,file="2012summary.csv",row.names=FALSE)
