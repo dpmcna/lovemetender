8677206a2320b9f74d5a9004f981573cbb400586
Ubuntu <ubuntu@ip-172-31-31-42.ap-southeast-2.compute.internal>
2013-06-01 17:06:11 +0000

    Built interface including comboboxes and hearts

--- utils.js
+++ utils.js
@@ -1,19 +1,16 @@
-function processData(allText) {
-    var allTextLines = allText.split(/\r\n|\n/);
-    var headers = allTextLines[0].split(',');
-    var lines = [];
+function sortInsensitive(a,b) {
+	if(a.toLowerCase() < b.toLowerCase()) return -1;
+	if(a.toLowerCase() > b.toLowerCase()) return 1;
+	return 0;
+}
 
-    for (var i=1; i<allTextLines.length; i++) {
-        var data = allTextLines[i].split(',');
-        if (data.length == headers.length) {
+function sortValue(a,b) {
+	if(a.Total < b.Total) return 1;
+	if(a.Total > b.Total) return -1;
+	return 0;
+}
 
-            var tarr = [];
-            for (var j=0; j<headers.length; j++) {
-                tarr.push(headers[j]+":"+data[j]);
-            }
-            lines.push(tarr);
-        }
-    }
-    // alert(lines);
+function commas(x){
+	return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
